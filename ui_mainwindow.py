77f030e3f20eeb74cb74d2dddec8f82ab57830d6
jiangruizhang <2109544929@qq.com>
2025-06-23 17:04:54 +0800

    修改主界面ui

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -16,40 +16,43 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QLabel, QListWidget, QListWidgetItem,
-    QMainWindow, QMenuBar, QSizePolicy, QStatusBar,
-    QTextEdit, QWidget)
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QTextEdit, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1061, 683)
+        MainWindow.resize(1064, 723)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.toselect = QListWidget(self.centralwidget)
         self.toselect.setObjectName(u"toselect")
-        self.toselect.setGeometry(QRect(20, 20, 241, 191))
+        self.toselect.setGeometry(QRect(20, 40, 241, 191))
         self.selected = QListWidget(self.centralwidget)
         self.selected.setObjectName(u"selected")
-        self.selected.setGeometry(QRect(20, 240, 241, 192))
+        self.selected.setGeometry(QRect(20, 260, 241, 192))
         self.toselectLabel = QLabel(self.centralwidget)
         self.toselectLabel.setObjectName(u"toselectLabel")
-        self.toselectLabel.setGeometry(QRect(20, 0, 58, 16))
+        self.toselectLabel.setGeometry(QRect(20, 20, 58, 16))
         self.selectedLabel = QLabel(self.centralwidget)
         self.selectedLabel.setObjectName(u"selectedLabel")
-        self.selectedLabel.setGeometry(QRect(20, 220, 58, 16))
+        self.selectedLabel.setGeometry(QRect(20, 240, 58, 16))
         self.courseInfoLable = QLabel(self.centralwidget)
         self.courseInfoLable.setObjectName(u"courseInfoLable")
-        self.courseInfoLable.setGeometry(QRect(20, 440, 58, 16))
+        self.courseInfoLable.setGeometry(QRect(20, 460, 58, 16))
         self.courseInfo = QTextEdit(self.centralwidget)
         self.courseInfo.setObjectName(u"courseInfo")
-        self.courseInfo.setGeometry(QRect(20, 460, 241, 141))
+        self.courseInfo.setGeometry(QRect(20, 480, 241, 141))
         self.courseInfo.setLineWrapMode(QTextEdit.LineWrapMode.NoWrap)
         self.courseInfo.setReadOnly(True)
+        self.addCourse = QPushButton(self.centralwidget)
+        self.addCourse.setObjectName(u"addCourse")
+        self.addCourse.setGeometry(QRect(20, 630, 100, 32))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1061, 37))
+        self.menubar.setGeometry(QRect(0, 0, 1064, 37))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -65,5 +68,6 @@ class Ui_MainWindow(object):
         self.toselectLabel.setText(QCoreApplication.translate("MainWindow", u"\u53ef\u9009\u8bfe\u7a0b", None))
         self.selectedLabel.setText(QCoreApplication.translate("MainWindow", u"\u5df2\u9009\u8bfe\u7a0b", None))
         self.courseInfoLable.setText(QCoreApplication.translate("MainWindow", u"\u8bfe\u7a0b\u4fe1\u606f", None))
+        self.addCourse.setText(QCoreApplication.translate("MainWindow", u"\u6dfb\u52a0\u8bfe\u7a0b", None))
     # retranslateUi
 
