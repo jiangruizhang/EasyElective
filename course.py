fed71292f93c0df48e61de7bd6915cf7c03a2ab7
jiangruizhang <2109544929@qq.com>
2025-06-23 22:38:29 +0800

    完善添加课程界面

---
+++ course.py
@@ -0,0 +1,18 @@
+class Course:
+    # TO ASK
+    # 上课时间 数字 和 星期几/第几节课 怎么对应的
+    # 目前认为是 1 - 7, 1 - 12
+
+    def __init__(self):
+        # 课程基本信息
+        self.schedule = []      # 上课时间，用 (day, ti) 来表示
+        self.point = 0          # 课程学分
+        self.must = False       # True 表示必须选择这门课
+        self.course = ''        # 课程名字
+        self.teacher = ''       # 老师名字
+        self.chosen = 0         # 已选人数
+        self.limit = 0          # 限选人数
+        self.selected = False   # 是否选择，默认为不选的状态
+        # 用户的情况
+        self.intension = 0      # 上这门课的意愿值，用 1 ～ 99 衡量
+    # TO THINK : 是否要区分 set 和 get ？
--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QTableWidgetItem
+from PySide6.QtCore import Signal, qDebug
 from ui_mainwindow import Ui_MainWindow 
 from ui_addcourse import Ui_AddCourse
+import pickle
+from pathlib import Path
+from course import Course
 
 class AddCourse(QDialog):
+    sendcourse = Signal(Course)
     def __init__(self, parent = None):
         super().__init__(parent)
         self.ui = Ui_AddCourse()
         self.ui.setupUi(self)
-        self.ui.finish.clicked.connect(self.accept)
+        for row in range(self.ui.schedule.rowCount()):
+            for col in range(self.ui.schedule.columnCount()):
+                if self.ui.schedule.item(row, col) is None:
+                    self.ui.schedule.setItem(row, col, QTableWidgetItem(""))
+        self.ui.finish.clicked.connect(self.newcourse)
+        self.ui.cancel.clicked.connect(self.close)
+    
+    def newcourse(self):
+        def getint(text, default = 0):
+            try:
+                return int(text)
+            except:
+                # QMessageBox.warning(None, "输入错误", "请输入有效整数")
+                return default
+        course = Course()
+        course.course = self.ui.course.toPlainText()
+        course.teacher = self.ui.teacher.toPlainText()
+        course.limit = getint(self.ui.teacher.toPlainText())
+        course.chosen = getint(self.ui.chosen.toPlainText())
+        course.intension = getint(self.ui.intension.toPlainText())
+        course.point = getint(self.ui.point.toPlainText())
+        course.must = self.ui.must.isChecked()
+        # qDebug(f'{len(self.ui.schedule.selectedItems())}')
+        for item in self.ui.schedule.selectedItems():
+            course.schedule.append((item.column() + 1, item.row() + 1))
+            # qDebug(f'{item.column() + 1} {item.row() + 1}')
+        self.sendcourse.emit(course)
+        self.close()
+        
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -19,7 +52,21 @@ class MainWindow(QMainWindow):
     
     def openAddCourse(self):
         self.addcourse = AddCourse(self)
+        self.addcourse.sendcourse.connect(self.newcourse)
         self.addcourse.exec()
+    def newcourse(self, course : Course):
+        pass
+        # qDebug('---new course---')
+        # qDebug(course.course)
+        # qDebug(course.teacher)
+        # for (day, time) in course.schedule:
+        #     qDebug(f'{day} {time}')
+    def load(self):
+        # 从文件读取所有课程信息
+        pass 
+    def save(self, course):
+        # 保存课程信息到文件
+        pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
