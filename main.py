ba55bb3d35c0df7f6bcd3e5dfed9f3bd714299b0
jiangruizhang <2109544929@qq.com>
2025-06-23 16:36:50 +0800

    新建py项目

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from ui_mainwindow import Ui_MainWindow  # 生成的UI类
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
